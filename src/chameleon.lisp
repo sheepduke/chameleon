21adb5a8b499842789cf33ee27aa8456e91a6147
YUE Daian <sheepduke@gmail.com>
2022-01-06 23:46:20 +0800

    feat: add docstring of generated function

--- src/chameleon.lisp
+++ src/chameleon.lisp
@@ -1,6 +1,7 @@
 (defpackage chameleon
   (:use #:cl)
   (:import-from #:alexandria
+                #:if-let
                 #:with-gensyms
                 #:symbolicate)
   (:export #:config
@@ -17,20 +18,26 @@
              (declare (ignore condition))
              (format stream "Profile is not set"))))
 
-(defgeneric get-config (profile key))
+(defgeneric get-config (profile key)
+  (:documentation "Get configuration instance with given PROFILE and KEY."))
 
 (defmethod get-config ((profile (eql nil)) key)
   (error 'null-profile-error))
 
-(defgeneric set-config (profile key value))
+(defgeneric set-config (profile key value)
+  (:documentation "Set configuration value with given PROFILE, KEY and VALUE."))
 
 (defmethod set-config ((profile (eql nil)) key value)
   (error 'null-profile-error))
 
 (defun make-keyword (thing)
+  "Make a keyword out of given string THING. The keyword is guaranteed
+to be upper case."
   (alexandria:make-keyword (string-upcase thing)))
 
 (defun config-item-to-slot (item)
+  "Convert given ITEM to CLOS slot. ITEM should be the a 2 or 3 length
+pair in config definition."
   (let ((name (first item))
         (value (second item))
         (doc (third item)))
@@ -39,10 +46,35 @@
                   :initform value)
             (if doc (list :documentation doc) nil))))
 
-(defun make-config-var-name (name)
-  (symbolicate "*CONFIG-" (string-upcase name) "*"))
-
 (defmacro defconfig (&body configs)
+  "Defines a configuration. The body CONFIGS contains multiple items,
+with each following this pattern:
+  (name initial-value &optional docstring)
+
+While calling this macro, all the given names, initial-values and
+docstrings are evaluated during macro expansion.
+
+Beneath the surface, DEFCONFIG actually generates the following stuff:
+
+1. A variable *PROFILE*. It stores the current profile which is
+   usually a keyword.
+
+2. A class named CONFIG. Each item maps to a slot definition:
+   - name maps to slot name.
+   - initial-value maps to :initform argument.
+   - docstring maps to :documentation property of the slot.
+
+3. A zero-arity function (and its setf version) for each name. The
+   function returns corresponding value according to *PROFILE*. When
+   the value itself is a function, it is called every time and the
+   value is returned. Otherwise, the value is directly returned.
+
+   This is useful for evaluating an expression every time.
+
+A typical example is:
+(defconfig
+  (server-port 5001 \"The server port.\")
+  (app-dir \"/tmp\"))"
   (let* ((profile-sym (symbolicate '*profile*))
          (config-sym (symbolicate 'config))
          (configs configs))
@@ -55,6 +87,9 @@
 
          ,@(mapcan (lambda (item)
                      `((defun ,(first item) ()
+                         ,(if-let (docstring (third item))
+                            docstring
+                            "")
                          (let ((,g-config (chameleon:get-config
                                            ,profile-sym
                                            ',(first item))))
@@ -67,9 +102,13 @@
                    configs)))))
 
 (defmacro defprofile (name &body configs)
+  "Defines a profile with given NAME. CONFIGS is one or more lists,
+with each following this pattern: (name value).
+
+The evaluation rule of value follows DEFCONFIG."
   (let* ((name name)
          (configs configs)
-         (config-var-name (make-config-var-name name)))
+         (config-var-name (symbolicate "*CONFIG-" (string-upcase name) "*")))
     (with-gensyms (g-profile g-key g-value)
       `(progn (defparameter ,config-var-name
                 (funcall #'make-instance
